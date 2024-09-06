b07ef9e125b4eb65568039a53090182efda1d163
Tao Zeng <TaoZeng@my.uopeople.edu>
2024-09-06 11:43:19 -0700

    Find most common character

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,30 @@
 import java.util.Scanner;  // Import the Scanner class
+import java.util.HashMap;
 
 public class Main {
-    private String userInput;
     static Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+    private String userInput;
 
     public static void main(String[] args) {
-        Main system = new Main();
-        system.run();
+        Main app = new Main();
+        app.run();
     }
 
     public void run() {
-        System.out.println("\nWelcome to use the text analysis tool!");
+        System.out.println("\nWelcome to Text Analysis Tool!");
         System.out.println("Please enter a paragraph or a lengthy text:");
 
         // nextLine() reads a String value from the user
         userInput = scanner.nextLine();  // Read user input
-        if (!userInput.isEmpty()) {
-            countCharacters();
-            countWords();
+
+        if (userInput == null || userInput.isEmpty()) {
+            throw new IllegalStateException("Please make sure to enter a paragraph or a lengthy text");
         }
+
+        countCharacters();
+        countWords();
+        findMostCommonCharacter(false);
+        findMostCommonCharacter(true);
     }
 
     public void countCharacters() {
@@ -31,5 +37,27 @@ public class Main {
     public void countWords() {
         String[] words = userInput.trim().split("\\s+");
         System.out.println("Total number of words: " + words.length);
+    }
+
+    public void findMostCommonCharacter(boolean includeSpaces) {
+        HashMap<Character, Integer> charFrequency = new HashMap<>();  // Create HashMap to store frequency of every character
+        int maxFrequency = 0;  // The frequency counting
+        char mostCommonChar = '\0';
+
+        for (int i = 0; i < userInput.length(); i++) {
+            // Push each character into map and increase its frequency
+            char c = userInput.charAt(i);
+
+            if (includeSpaces || !Character.isWhitespace(c)) {
+                charFrequency.put(c, charFrequency.getOrDefault(c, 0) + 1);
+
+                if(charFrequency.get(c) > maxFrequency) {
+                    maxFrequency = charFrequency.get(c);
+                    mostCommonChar = c;
+                }
+            }
+        }
+
+        System.out.println("The most common character " + (includeSpaces ? "(including spaces): " : "(excluding spaces): ") + "'" + mostCommonChar + "'");
     }
 }
